80863d95a816395c01cdf679ed0bcf317d4b2101
Katarzyna Szymanska <katarzyna.szymanska@zoho.eu>
2022-10-23 18:35:41 +0200

    Initial commit

---
+++ project.cpp
@@ -0,0 +1,16 @@
+#include "project.hpp"
+
+int main()
+
+{
+int a =0;
+int b =0;
+
+cout<<"enter first number:\n";
+cin>>a;
+cout<<"enter second number:\n";
+cin>>b;
+cout<<sum(a,b)<<endl;
+
+return 0;
+}
